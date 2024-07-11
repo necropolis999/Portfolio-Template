4ede9070abaf1164e5f22c49d766c3375d0188bb
necropolis999 <stephen@silverglassworks.com>
2024-07-11 11:46:29 -0400

    Update Hero.jsx
    removed portrait, changed name and hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,12 +42,12 @@ const Hero = () => {
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
-                Shaquille
+                Stephen
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
-              consectetur adipisicing elit deleniti, voluptas.
+              Making your applications, <br className="sm:block hidden" />
+              your sites, and your business grow.
             </p>
           </div>
           <div
@@ -82,13 +82,7 @@ const Hero = () => {
 
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
         <div>
-          <img
-            className="absolute bottom-0 ml-[50vw] 
-            lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
-            sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
-            alt="shaquille"
-          />
+          
         </div>
       </section>
     </>
